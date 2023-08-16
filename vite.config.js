4161d9bddd5e8568da8be3b9e5825f55fd45d505
pratamamp <pratamamp@gmail.com>
2023-08-16 14:30:19 +0700

    update pwa setup

--- vite.config.js
+++ vite.config.js
@@ -2,46 +2,41 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
 
-export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
-      manifest: {
-        name: "weatherapp",
-        short_name: "weatherapp",
-        description: "Demo Weather Simulation - Arcgis Javascript Map SDK",
-        theme_color: "#ffffff",
-        start_url: "/",
-        workbox: {
-          globalPatterns: ["**/*.{js,css,html,ico,png,svg}"],
-        },
-        icons: [
-          {
-            src: "pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-          {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any",
-          },
-          {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable",
-          },
-        ],
+const manifestForPlugin = {
+  registerType: "prompt",
+  includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+  manifest: {
+    name: "weatherapp",
+    short_name: "weatherapp",
+    description: "Demo Weather Simulation - Arcgis Javascript Map SDK",
+    theme_color: "#ffffff",
+    start_url: "/",
+    icons: [
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
       },
-    }),
-  ],
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "apple touch icon",
+      },
+      {
+        src: "/maskable_icon.png",
+        sizes: "225x225",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+  },
+};
+export default defineConfig({
+  plugins: [react(), VitePWA(manifestForPlugin)],
 });
